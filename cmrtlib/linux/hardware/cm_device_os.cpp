758261b78ff8b43b1f28f755ad973b7dc67d02c1
Andreas Huber <andih@google.com>
2024-07-15 04:29:11 -0700

    Initial import of "intel-media-driver", an Intel(R) support library for VAAPI.
    This enables development of hardware-accelerated media codecs of
    VAAPI-supported hardware.
    
    Test: verified it builds on aosp-main-future only, mapped into device-specific branch and functionally tested and verified there.
    Third-Party Import of: https://github.com/intel/media-driver
    Request Document: go/android3p
    For CL Reviewers: go/android3p#reviewing-a-cl
    For Build Team: go/ab-third-party-imports
    Bug: http://b/352643499
    Original import of the code can be found at: https://googleplex-android.googlesource.com/platform/external/intel-media-driver/+/refs/heads/third-party-review.
    Security Questionnaire: http://b/352643499#comment1
    
    Change-Id: I1755a8fb2327c579fc47123f05c216bce5de6b84

--- cmrtlib/linux/hardware/cm_device_os.cpp
+++ cmrtlib/linux/hardware/cm_device_os.cpp
@@ -493,8 +493,10 @@ CmDevice_RT::CmDevice_RT(
     m_gtpinBufferUP1(nullptr),
     m_gtpinBufferUP2(nullptr),
     m_createOption(createOption),
-    m_driverStoreEnabled(0),
-    m_driFileDescriptor(0)
+    m_driverStoreEnabled(0)
+#if !defined(ANDROID)
+    ,m_driFileDescriptor(0)
+#endif
 {
 
     // New Surface Manager
